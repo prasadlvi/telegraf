8fe419af6774417a3cbf367656c3268108c9ea3b
Prasad <prasad@lvi.co.jp>
2020-04-08 14:43:07 +0900

    add syslog plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -146,7 +146,7 @@ import (
 	//_ "github.com/influxdata/telegraf/plugins/inputs/suricata"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/swap"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/synproxy"
-	//_ "github.com/influxdata/telegraf/plugins/inputs/syslog"
+	_ "github.com/influxdata/telegraf/plugins/inputs/syslog"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/sysstat"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/system"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/systemd_units"
