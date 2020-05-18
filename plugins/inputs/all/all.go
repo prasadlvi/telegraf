707838ac9b66fb414c512ed66b0520955b0e16ca
Prasad <prasad@lvi.co.jp>
2020-05-18 10:34:20 +0900

    test

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -43,7 +43,7 @@ import (
 	//_ "github.com/influxdata/telegraf/plugins/inputs/exec"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/fail2ban"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/fibaro"
-	//_ "github.com/influxdata/telegraf/plugins/inputs/file"
+	_ "github.com/influxdata/telegraf/plugins/inputs/file"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/filecount"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/filestat"
 	//_ "github.com/influxdata/telegraf/plugins/inputs/fireboard"
